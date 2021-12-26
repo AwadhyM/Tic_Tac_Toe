1d62ce74ff79e21c24d63131acf7c390d0a2e1ed
awadhy <awadhym@outlook.com>
2021-12-26 20:42:50 +0000

    Written up code that will create player objects

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -7,6 +7,10 @@ class Game
     @turn = 0
     @counter = 0
   end
+
+  def fetch_player
+    puts "\nGreetings player#{@@number_of_players}, what username would you like for this game?"
+  end
 end
 
 class GameBoard
@@ -25,3 +29,17 @@ class GameBoard
     end
   end
 end
+
+class Players
+  attr_accessor :name, :move
+
+  @@number_of_players = 0
+
+  def initialize(name, move)
+    @name = name
+    @move = move
+    @@number_of_players += 1
+  end
+end
+
+game = Game.new 
