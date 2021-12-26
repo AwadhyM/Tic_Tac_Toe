a5d8fb2c19dd814d4b61c2562c10995270c9fff8
awadhy <awadhym@outlook.com>
2021-12-26 12:03:51 +0000

    Refactored code written so far so it is more in line with conventional Ruby styling standards

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,32 +1,27 @@
-class Game 
+# frozen_string_literal: true
 
+class Game
   attr_accessor :turn, :counter
 
   def initialize
     @turn = 0
     @counter = 0
   end
-end 
+end
 
-game = Game.new
-
-class GameBoard 
-  
+class GameBoard
   def initialize
     @board = [
-               ['','',''],
-               ['','',''],
-               ['','','']
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', '']
     ]
   end
 
-    def display_board
-      @board.each do |row|
-        row.each {|cell| print "[ #{cell} ]" }
-        puts "\n--------------"
-      end
+  def display_board
+    @board.each do |row|
+      row.each { |cell| print "[ #{cell} ]" }
+      puts "\n--------------"
+    end
   end
 end
-
-  board = GameBoard.new 
-  board.display_board
