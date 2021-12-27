8212aa03cdfb889f7252e56a5c8ce6243bbe1413
awadhy <awadhym@outlook.com>
2021-12-27 12:21:48 +0000

    Created beginnings of playround loop. So far loop asks the player for coordinates and displays board after every turn

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -50,11 +50,30 @@ class Game
     puts "\nGreetings player#{@counter}, what username would you like for this game?"
   end
 
+  def retrieve_x_coordinate
+    puts "enter the x coordinate in which you would like to place your move"
+  end
+
+  def retrieve_y_coordinate
+    puts "enter the y coordinate in which you would like to place your move"
+  end
+
   def display_info(player1, player2)
     puts 'Game Info:'
     puts "\nUsername:#{player1.name} |Move: #{player1.move}"
-    puts "\Username:n#{player2.name} |Move: #{player2.move}"
+    puts "\nUsername:n#{player2.name} |Move: #{player2.move}"
   end
+
+  def play_round (board, game, player1, player2)
+    while @turn <= 7 do
+    board.display_board
+    game.retrieve_x_coordinate
+    x = gets.chomp
+    game.retrieve_y_coordinate
+    y = gets.chomp
+    @turn += 1
+  end
+end
 end
 
 game = Game.new
@@ -67,3 +86,4 @@ game.ask_player
 player2 = Players.new(gets.chomp.to_s, 'X')
 player2.verify_name
 game.display_info(player1, player2)
+game.play_round(board, game, player1, player2)
